9e633808423e3c99eae041ccd2f4f6551c0a4650
Arda999-prog <ardannyromero@gmail.com>
2025-07-27 19:06:49 -0500

    Sin cambios. Solo revision de Git

--- src/DAO/ConexionBD.java
+++ src/DAO/ConexionBD.java
@@ -7,14 +7,12 @@ import java.sql.SQLException;
 public class ConexionBD {
 
     // --- DATOS DE TU CONEXIÓN SQL SERVER ---
-    // ¡REEMPLAZA ESTOS DATOS CON LOS DE TU SERVIDOR SQL SERVER!
     private static final String HOST = "sibibli-server.database.windows.net"; // O la IP/nombre de tu servidor
     private static final String PUERTO = "1433";
     private static final String BASE_DE_DATOS = "SIBIBLI"; // El nombre de tu base de datos
     private static final String USUARIO = "mtard"; // Tu usuario de SQL Server
     private static final String CONTRASENA = "Mt4rd1234"; // Tu contraseña de SQL Server
 
-    // <-- CAMBIO: La URL para SQL Server tiene un formato diferente.
     // Opciones como 'encrypt=true' y 'trustServerCertificate=true' son comunes para evitar problemas de SSL.
     private static final String URL = "jdbc:sqlserver://" + HOST + ":" + PUERTO + ";databaseName=" + BASE_DE_DATOS + ";encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
